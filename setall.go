4947f1eb28b3c6f43e2d373a71ec20884b9e1c18
aktungmak <aktungmak@gmail.com>
2015-03-10 15:45:56 +0000

    ***VERSION 1.0*** This has been released to the US team for their testing, in preparation for delivery to the customer. Everything works fine after my testing, and changes in this commit make it a lot more robust to dodgy config files and network issues.

--- setall.go
+++ setall.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"gopkg.in/xmlpath.v2"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strings"
@@ -43,40 +44,48 @@ func NewMethodCall(mname, xpath, value string) MethodCall {
 	return mc
 }
 
-func MakeSetParamRequestTo(ipa, xpath, value string) (string, error) {
-	mc := NewMethodCall("setParameters", xpath, value)
+func MakeSetParamRequestTo(ipa, xpath, value string) (msg string, err error) {
+	// generate an XML string of the RPC
+	mc := NewMethodCall("setBoxParameters", xpath, value)
 	ox, err := xml.MarshalIndent(mc, "", "  ")
 	if err != nil {
 		return "", err
 	}
-	// fmt.Printf("%s%s", xml.Header, ox)
+
+	// send the POST request and check for HTTP errors
 	tgturi := fmt.Sprintf("http://%s/xmlrpc.cgi", ipa)
 	body := bytes.NewBuffer(ox)
 	r, err := http.Post(tgturi, "text/xml", body)
 	if err != nil {
-		var reason string
-		if err.Timeout() {
-			reason = "timed out"
-		} else {
-			reason = "request failed"
-		}
-		return "", errors.New(reason)
+		return "", err
 	}
 
+	// parse the response and check for parse errors
 	root, err := xmlpath.Parse(r.Body)
 	if err != nil {
 		return "", err
 	}
-	resultpath := xmlpath.MustCompile("//string")
-	if value, ok := resultpath.String(root); ok {
-		if len(value) == 0 {
-			return value, nil
+
+	// examine the response and look for RPC errors
+	faultpath := xmlpath.MustCompile("//fault/value/string")
+	parampath := xmlpath.MustCompile("//param/value/string")
+
+	if msg, found := parampath.String(root); found {
+		// it maybe worked
+		if len(msg) == 0 {
+			// success!
+			err = error(nil)
 		} else {
-			return "", errors.New(value)
+			// there was a problem
+			err = errors.New(msg)
 		}
+	} else if msg, found := faultpath.String(root); found {
+		//it didn't work
+		err = errors.New(msg)
 	} else {
-		return "", errors.New("couldn't parse response!")
+		err = errors.New("couldn't parse response!")
 	}
+	return msg, err
 }
 
 func ParseAddrFile(fname string) ([]string, error) {
@@ -90,7 +99,11 @@ func ParseAddrFile(fname string) ([]string, error) {
 	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		txt := scanner.Text()
+		isip := net.ParseIP(txt)
+		if isip != nil {
+			lines = append(lines, txt)
+		}
 	}
 
 	return lines, scanner.Err()
@@ -108,7 +121,9 @@ func ParseXpathFile(fname string) ([][]string, error) {
 
 	for scanner.Scan() {
 		pair := strings.SplitN(scanner.Text(), ",", 2)
-		params = append(params, pair)
+		if len(pair) == 2 {
+			params = append(params, pair)
+		}
 	}
 
 	return params, scanner.Err()
@@ -116,27 +131,45 @@ func ParseXpathFile(fname string) ([][]string, error) {
 
 func main() {
 	var ipafile, xpathfile string
-	flag.StringVar(&ipafile, "a", "",
+	var debugmode bool
+	flag.StringVar(&ipafile, "a", "REQUIRED",
 		"file containing list of unit IP addresses, one per line")
-	flag.StringVar(&xpathfile, "x", "",
-		"file containing list of xpath, value pairs, one pair per line")
+	flag.StringVar(&xpathfile, "c", "REQUIRED",
+		"file containing list of xpath, value command pairs, one pair per line")
+	flag.BoolVar(&debugmode, "d", false,
+		"enables debug mode, for extra detail of RPCs being sent")
 	flag.Parse()
 
+	if xpathfile == "REQUIRED" || ipafile == "REQUIRED" {
+		log.Fatal("Incorrect usage, use --help for more information.\n\n")
+	}
+
+	// read the configuration files provided
 	ipas, err := ParseAddrFile(ipafile)
 	xpaths, err := ParseXpathFile(xpathfile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, xp := range xpaths {
-		for _, ipa := range ipas {
+	// now, do the actual work of setting values!
+	errcnt := 0
+	for i, xp := range xpaths {
+		for j, ipa := range ipas {
 			res, err := MakeSetParamRequestTo(ipa, xp[0], xp[1])
+			if debugmode {
+				fmt.Printf("\n>>>%s\n", res)
+			}
 			if err != nil {
+				errcnt += 1
+				fmt.Println("")
 				fmt.Println(xp, ipa, "ERROR", err)
 			} else {
-				fmt.Println(xp, ipa, "SET OK", res)
+				fmt.Printf("\rSetting param %d of %d on unit %d of %d...    ",
+					i+1, len(xpaths), j+1, len(ipas))
+
 			}
 		}
 	}
-
+	fmt.Println("")
+	fmt.Printf("Completed with %d errors.\n\n", errcnt)
 }
